98a1da04dc41e10bef815f27fe2fa6f1fa169966
mreigen <minhtrietle@yahoo.com>
2012-11-18 17:07:44 -0800

    Add scope by_...

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,6 @@
 class PostsController < ApplicationController
   def index
-    @posts = Post.last(10)
+    @posts = Post.by_month(Time.new.month).last(10)
     respond_to do |format|
       format.html
     end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,12 @@
 class Post < ActiveRecord::Base
   has_one :user
   has_one :icon
+  
+  scope :by_month, (lambda do |month|
+    where("extract(month from created_at) = ?", month) unless month.blank?
+  end)
+  
+  scope :by_date, (lambda do |year, month, day|
+    where("extract(year from created_at) = ? AND extract(month from created_at) = ? AND extract(day from created_at) = ?", year, month, day)
+  end)
 end
