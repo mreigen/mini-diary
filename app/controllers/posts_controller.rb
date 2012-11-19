81619c70d5567df98f5d7599269e889f9a303c03
mreigen <minhtrietle@yahoo.com>
2012-11-19 00:59:15 -0800

    first commit for new post page
    pretty coool :)

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,8 +1,21 @@
 class PostsController < ApplicationController
+  
+  def new
+    
+  end
+  
   def index
     @posts = Post.by_month(Time.new.month).last(10)
     respond_to do |format|
       format.html
     end
+  end
+  
+  def new_post
+    params.merge!({
+      :user_id => current_user.id
+    })
+    p = Post.create(params)
+    p.save
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ VideoCloud::Application.routes.draw do
   devise_for :users
   
   resources :users
+  resources :posts
   
   root :to => "posts#index"
 end
